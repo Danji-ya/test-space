a25c616f2f4bba2e0b5e7ef5757d73655edb31bf
Danji-ya <erangerang15@gmail.com>
2024-12-22 10:52:38 +0900

    feat: prefetch 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import Todos from "./components/Todos";
+import { getTodos } from "./components/Todos/useTodos";
 
 export default async function TodosPage() {
-  return <Todos />;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
+
+  return (
+    // Neat! Serialization is now as easy as passing props.
+    // HydrationBoundary is a Client Component, so hydration will happen there.
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Todos />
+    </HydrationBoundary>
+  );
 }
